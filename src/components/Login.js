edd9ea13c3086dfe2f5467ed1b409da43039f448
ardelrio <alonsorafaeldelrio@gmail.com>
2020-03-17 16:32:35 -0500

    Added form input handling for Login and SignUp forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,9 +9,18 @@ import { withRouter } from 'react-router-dom';
 export default class Login extends React.Component {
   static contextType = StoreContext;
 
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
   onLogin = () => {
     const { userStore } = this.context;
     const { history } = this.props;
+
+    //alert('Email: ' + this.state.email + '\n' + 'Password: ' + this.state.password);
 
     // this is fake login, should replace with firebase.auth
     userStore.currentUser = {
@@ -22,16 +31,38 @@ export default class Login extends React.Component {
     history.replace('/');
   };
 
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <h2>hi I'm login</h2>
         <form id="loginForm">
-          <label for="email">Email: </label>
-          <input type="text" id="email" name="email" placeholder="Email" />
+          <label>Email: </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
           <br />
-          <label for="password">Password: </label>
-          <input type="password" id="password" name="password" placeholder="Password" />
+          <label>Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
           <br />
           <Button onClick={this.onLogin} type="primary">
             LOGIN
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,9 +9,32 @@ import { withRouter } from 'react-router-dom';
 export default class SignUp extends React.Component {
   static contextType = StoreContext;
 
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', verifypassword: '', displayname: '' };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleVerifyPasswordChange = this.handleVerifyPasswordChange.bind(this);
+    this.handleDisplayNameChange = this.handleDisplayNameChange.bind(this);
+  }
+
   onSignup = () => {
     const { userStore } = this.context;
     const { history } = this.props;
+
+    /*alert(
+      'Email: ' +
+        this.state.email +
+        '\n' +
+        'Password: ' +
+        this.state.password +
+        '\n' +
+        'Verify Password: ' +
+        this.state.verifypassword +
+        '\n' +
+        'Display Name: ' +
+        this.state.displayname
+    );*/
 
     // this is fake signup, should replace with firebase.auth
     userStore.currentUser = {
@@ -22,22 +45,66 @@ export default class SignUp extends React.Component {
     history.replace('/');
   };
 
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleVerifyPasswordChange(event) {
+    this.setState({ verifypassword: event.target.value });
+  }
+
+  handleDisplayNameChange(event) {
+    this.setState({ displayname: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <h2>hi I'm Signup</h2>
         <form id="signUpForm">
-          <label for="email">Email: </label>
-          <input type="text" id="email" name="email" placeholder="Email" />
+          <label>Email: </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
           <br />
-          <label for="displayname">Display Name: </label>
-          <input type="text" id="displayname" name="displayname" placeholder="Display Name" />
+          <label>Display Name: </label>
+          <input
+            type="text"
+            id="displayname"
+            name="displayname"
+            placeholder="Display Name"
+            value={this.state.displayname}
+            onChange={this.handleDisplayNameChange}
+          />
           <br />
-          <label for="password">Password: </label>
-          <input type="password" id="password" name="password" placeholder="Password" />
+          <label>Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
           <br />
-          <label for="verifypassword">Verify Password: </label>
-          <input type="password" id="verifypassword" name="verifypassword" placeholder="Password" />
+          <label>Verify Password: </label>
+          <input
+            type="password"
+            id="verifypassword"
+            name="verifypassword"
+            placeholder="Verify Password"
+            value={this.state.verifypassword}
+            onChange={this.handleVerifyPasswordChange}
+          />
           <br />
           <Button onClick={this.onSignup} type="primary">
             SIGN UP

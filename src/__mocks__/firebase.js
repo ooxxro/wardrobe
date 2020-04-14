5bacc282c0b90a3d311f893325c979b13f486a86
Amber Hung <yhung32@wisc.edu>
2020-04-13 23:44:31 -0500

    add mock firebase and test coverage

--- src/__mocks__/firebase.js
+++ src/__mocks__/firebase.js
@@ -1,19 +1,48 @@
-const firebase = {
-  auth: () => ({
-    onAuthStateChanged: jest.fn(),
-    currentUser: {
-      displayName: 'testDisplayName',
-      email: 'test@test.com',
-      emailVerified: true,
-    },
-    sendPasswordResetEmail: jest.fn(() => Promise.resolve()),
-    createUserWithEmailAndPassword: jest.fn(() =>
-      Promise.resolve('result of createUserWithEmailAndPassword')
-    ),
-    signInWithEmailAndPassword: jest.fn(() =>
-      Promise.resolve('result of signInWithEmailAndPassword')
-    ),
-  }),
-};
+const firebasemock = require('firebase-mock');
 
-export default firebase;
+const mockauth = new firebasemock.MockAuthentication();
+// const mockdatabase = new firebasemock.MockFirebase();
+const mockfirestore = new firebasemock.MockFirestore();
+const mockstorage = new firebasemock.MockStorage();
+// const mockmessaging = new firebasemock.MockMessaging();
+const mocksdk = new firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  null,
+  // use null if your code does not use AUTHENTICATION
+  () => {
+    return mockauth;
+  },
+  // use null if your code does not use FIRESTORE
+  () => {
+    return mockfirestore;
+  },
+  // use null if your code does not use STORAGE
+  () => {
+    return mockstorage;
+  },
+  // use null if your code does not use MESSAGING
+  null
+  // () => {
+  //   return mockmessaging;
+  // }
+);
+
+// const firebase = {
+//   auth: () => ({
+//     onAuthStateChanged: jest.fn(),
+//     currentUser: {
+//       displayName: 'testDisplayName',
+//       email: 'test@test.com',
+//       emailVerified: true,
+//     },
+//     sendPasswordResetEmail: jest.fn(() => Promise.resolve()),
+//     createUserWithEmailAndPassword: jest.fn(() =>
+//       Promise.resolve('result of createUserWithEmailAndPassword')
+//     ),
+//     signInWithEmailAndPassword: jest.fn(() =>
+//       Promise.resolve('result of signInWithEmailAndPassword')
+//     ),
+//   }),
+// };
+
+export default mocksdk;
--- src/components/Setting.test.js
+++ src/components/Setting.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render } from 'test-utils';
 import Setting from './Setting';
+import userStore from '../stores/UserStore';
 
 it('renders Setting without crashing', () => {
+  // mock userStore login
+  userStore.setUser({
+    uid: 'testUid',
+    email: 'mocktest@wardrobe.rocks',
+    displayName: 'Mock Test',
+    photoURL: null,
+  });
+
   const { container } = render(<Setting />);
   expect(container.innerHTML).toMatch('Settings');
 });
